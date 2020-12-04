cd98b97889d5dc3bce48e18161d57c8658c04087
fancystraw21 <fancystraw21@gmail.com>
2020-12-04 16:06:34 +0800

    tt

--- speedall.sh
+++ speedall.sh
@@ -7,11 +7,11 @@ export PATH
 #授权文件自动生成url
 APX=http://rs.91yun.pw/apx1.php
 #安装包下载地址
-INSTALLPACK=https://github.com/91yun/serverspeeder/blob/test/91yunserverspeeder.tar.gz?raw=true
+INSTALLPACK=https://github.com/fancystraw21/serverspeeder/blob/master/91yunserverspeeder.tar.gz?raw=true
 #判断版本支持情况的地址
-CHECKSYSTEM=https://raw.githubusercontent.com/91yun/serverspeeder/test/serverspeederbin.txt
+CHECKSYSTEM=https://github.com/fancystraw21/serverspeeder/blob/master/serverspeederbin.txt
 #bin下载地址
-BIN=downloadurl
+BINURL=http://rs.91yun.pw/
 
 
 
@@ -19,193 +19,122 @@ BIN=downloadurl
 Get_Dist_Name()
 {
     if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-        DISTRO='CentOS'
+        release='CentOS'
         PM='yum'
     elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-        DISTRO='Debian'
+        release='Debian'
         PM='apt'
     elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-        DISTRO='Ubuntu'
-        PM='apt'
-    elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
-        DISTRO='Raspbian'
-        PM='apt'
+        release='Ubuntu'
+        PM='apt'		
 	else
-        DISTRO='unknow'
+        release='unknow'
     fi
-    Get_OS_Bit
+    
 }
 
 Get_OS_Bit()
 {
     if [[ `getconf WORD_BIT` = '32' && `getconf LONG_BIT` = '64' ]] ; then
-        ver3='x64'
+        bit='x64'
     else
-        ver3='x32'
+        bit='x32'
     fi
 }
 
 Get_Dist_Name
+Get_OS_Bit
+kernel=`uname -r`
+kernel_result=""
 
-#安装相应的软件
-if [ "$DISTRO" == "CentOS" ];then
-	yum install -y redhat-lsb curl net-tools
-elif [ "$DISTRO" == "Debian" ];then
-	apt-get update
-	apt-get install -y lsb-release curl
-elif [ "$DISTRO" == "Raspbian" ];then
-	apt-get update
-	apt-get install -y lsb-release curl
-elif [ "$DISTRO" == "Ubuntu" ];then
-	apt-get update
-	apt-get install -y lsb-release curl
+echo -e "\r\n"
+echo "===============System Info======================="
+echo "$release "
+echo "$kernel "
+echo "$bit "
+echo "================================================="
+echo -e "\r\n"
+
+#下周支持的内核库
+wget $CHECKSYSTEM --no-check-certificate -O serverspeederbin.txt || { echo "Error downloading file, please try again later.";exit 1; }
+
+#判断是否有完全匹配的内核
+grep -q "$release/[^/]*/$kernel/$bit" serverspeederbin.txt
+if [ $? -eq 0 ]; then
+	#如果完全匹配，则取的内核版本
+	kernel_result=$kernel
 else
-	echo "一键脚本暂时只支持centos，ubuntu和debian的安装，其他系统请选择手动安装http://www.91yun.org/serverspeeder91yun"
+	#如果没有完全匹配的内核，则开始模糊匹配
+	echo ">>>This kernel is not supported. Trying fuzzy matching..."
+	echo -e "\r\n"
+	#因为centos和ubuntu的版本号不太一样，所以centos匹配2.6.32-504.el6.x86_64到504 ，
+	if [ "$release" == "CentOS" ]; then
+		kernel1=`echo $kernel | awk -F '-' '{ print $1 }'`
+		kernel2=`echo $kernel | awk -F '-' '{ print $2 }' | awk -F '.' '{ print $1 }'`
+	elif [[ "$release" == "Ubuntu" ]] || [[ "$release" == "Debian" ]]; then
+		kernel1=`echo $kernel | awk -F '-' '{ print $1 }'`
+		kernel2=`echo $kernel | awk -F '-' '{ print $2 }'`
+	else
+		echo "This script only supports CentOS, Ubuntu and Debian."
+		exit 1
+	fi
+	
+	grep -q "$release/[^/]*/$kernel1\(-\)\{0,1\}$kernel2[^/]*/$bit" serverspeederbin.txt
+	if [ $? -eq 1 ]; then
+			echo -e "\r\n"
+			echo -e "Serverspeeder is not supported on this kernel! View all supported systems and kernels here:\033[41;37m https://www.91yun.org/serverspeeder91yun \033[0m"
+			exit 1
+	else
+		#如果模糊匹配到了，就给玩家选
+		echo "There is no exact match for this kernel, please choose the closest one below:"
+		echo -e "The current kernel is \033[41;37m $kernel \033[0m"
+		echo -e "\r\n"
+		cat serverspeederbin.txt | grep  "$release/[^/]*/$kernel1\(-\)\{0,1\}$kernel2[^/]*/$bit"  | awk -F '/' '{ print NR"："$3 }'
+		echo -e "\r\n"
+		echo "Please enter the number of your option："	
+		read cver2
+		if [ "$cver2" == "" ]; then
+			echo "You did not choose any kernel options. Installation terminated."
+			exit 1
+		fi
+		echo -e "\r\n"
+		cver2str="cat serverspeederbin.txt | grep  \"$release/[^/]*/$kernel1\(-\)\{0,1\}$kernel2[^/]*/$bit\"  | awk -F '/' '{ print NR\"：\"\$3 }' | awk -F '：' '/"$cver2："/{ print \$2 }' | awk 'NR==1{print \$1}'"
+		kernel_result=$(eval $cver2str)			
+	fi
+fi
+
+if [ "$kernel_result" == "" ]; then
+	echo "Unable to get kernel information. Installtion terminated."
 	exit 1
 fi
 
-release=$DISTRO
-#发行版本
-if [ "$release" == "Debian" ]; then
-	ver1str="lsb_release -rs | awk -F '.' '{ print \$1 }'"
-else
-	ver1str="lsb_release -rs | awk -F '.' '{ print \$1\".\"\$2 }'"
-fi
-ver1=$(eval $ver1str)
-#ver11=`echo $ver1 | awk -F '.' '{ print $1 }'`
-
-#内核版本
-ver2=`uname -r`
-#锐速版本
-ver4=3.10.61.0
-
-echo "================================================="
-echo "操作系统：$release "
-echo "发行版本：$ver1 "
-echo "内核版本：$ver2 "
-echo "位数：$ver3 "
-echo "锐速版本：$ver4 "
-echo "================================================="
+echo "Installing ServerSpeeder, please wait for a moment..."
 
 
-#下载支持的bin列表
-curl "http://soft.91yun.org/soft/serverspeeder/serverspeederbin.txt" -o serverspeederbin.txt || { echo "文件下载失败，自动退出，可以前往http://www.91yun.org/serverspeeder91yun手动下载安装包";exit 1; }
+#开始匹配锐速的版本
+serverspeederver=3.10.61.0
 
-
-
-
-#判断内核版本
-grep -q "$release/$ver11[^/]*/$ver2/$ver3" serverspeederbin.txt
+grep -q "$release/[^/]*/$kernel_result/$bit/$serverspeederver" serverspeederbin.txt
 if [ $? == 1 ]; then
-		#echo "没有找到内核"
-	if [ "$release" == "CentOS" ]; then
-		ver21=`echo $ver2 | awk -F '-' '{ print $1 }'`
-		ver22=`echo $ver2 | awk -F '-' '{ print $2 }' | awk -F '.' '{ print $1 }'`
-		#cat serverspeederbin.txt | grep -q  "$release/$ver1/$ver21-$ver22[^/]*/$ver3/"
-		cat serverspeederbin.txt | grep -q  "$release/$ver11[^/]*/$ver21-$ver22[^/]*/$ver3/"
-
-		if [ $? == 1 ]; then
-			echo -e "\r\n"
-			echo "锐速暂不支持该内核，程序退出.自动安装判断比较严格，你可以到http://www.91yun.org/serverspeeder91yun手动下载安装文件尝试不同版本"
-			exit 1
-		fi
-		echo "没有完全匹配的内核，请选一个最接近的尝试，不确保一定成功,(如果有版本号重复的选项随便选一个就可以)"
-		echo -e "您当前的内核为 \033[41;37m $ver2 \033[0m"
-		cat serverspeederbin.txt | grep  "$release/$ver11[^/]*/$ver21-$ver22[^/]*/$ver3/"  | awk -F '/' '{ print NR"："$3 }'
-	fi
-
-
-	if [[ "$release" == "Ubuntu" ]] || [[ "$release" == "Debian" ]]; then
-		ver21=`echo $ver2 | awk -F '-' '{ print $1 }'`
-		ver22=`echo $ver2 | awk -F '-' '{ print $2 }'`
-		cat serverspeederbin.txt | grep -q  "$release/$ver11[^/]*/$ver21(-)?$ver22[^/]*/$ver3/"
-
-		if [ $? == 1 ]; then
-			echo -e "\r\n"
-			echo "锐速暂不支持该内核，程序退出.自动安装判断比较严格，你可以到http://www.91yun.org/serverspeeder91yun手动下载安装文件尝试不同版本"
-			exit 1
-		fi
-		echo "没有完全匹配的内核，请选一个最接近的尝试，不确保一定成功,(如果有版本号重复的选项随便选一个就可以)"
-		echo -e "您当前的内核为 \033[41;37m $ver2 \033[0m"
-		cat serverspeederbin.txt | grep  "$release/$ver11[^/]*/$ver21(-)?$ver22[^/]*/$ver3/"  | awk -F '/' '{ print NR"："$3 }'
-	fi
-
-
-	echo "请选择（输入数字序号）："
-	read cver2
-	if [ "$cver2" == "" ]; then
-		echo "未选择任何内核版本，脚本退出"
-		exit 1
-	fi
-
-	if [ "$release" == "CentOS" ]; then
-		cver2str="cat serverspeederbin.txt | grep  \"$release/$ver11[^/]*/$ver21-$ver22[^/]*/$ver3/\"  | awk -F '/' '{ print NR\"：\"\$3 }' | awk -F '：' '/"$cver2："/{ print \$2 }' | awk 'NR==1{print \$1}'"
-	fi
-	if [[ "$release" == "Ubuntu" ]] || [[ "$release" == "Debian" ]]; then
-		cver2str="cat serverspeederbin.txt | grep  \"$release/$ver11[^/]*/$ver21-[^/]*/$ver3/\"  | awk -F '/' '{ print NR\"：\"\$3 }' | awk -F '：' '/"$cver2："/{ print \$2 }' awk 'NR==1{print \$1}'"
-	fi
-	ver2=$(eval $cver2str)
-	if [ "$ver2" == "" ]; then
-        echo "脚本获得不了内核版本号，错误退出"
-		exit 1
-    fi
-	#根据所选的内核版本，再回头确定大版本
-
-fi
-#判断锐速版本
-grep -q "$release/$ver1/$ver2/$ver3/$ver4" serverspeederbin.txt
-if [ $? == 1 ]; then
-	grep -q "$release/$ver11[^/]*/$ver2/$ver3/$ver4" serverspeederbin.txt
-	if [ $? == 1 ]; then
-		echo -e "\r\n"
-		echo -e "我们用的锐速安装文件是\033[41;37m 3.10.60.0  \033[0m，但这个内核没有匹配的，请选择一个接近的锐速版本号尝试，不确保一定可用,(如果有版本号重复的选项随便选一个就可以)"
-		cat serverspeederbin.txt | grep  "$release/$ver11[^/]*/$ver2/$ver3/"  | awk -F '/' '{ print NR"："$5 }'
-		echo "请选择锐速版本号（输入数字序号）："
-			read cver4
-		if [ "$cver4" == "" ]; then
-			echo "未选择任何锐速版本，脚本退出"
-			exit 1
-		fi
-			cver4str="cat serverspeederbin.txt | grep  \"$release/$ver11[^/]*/$ver2/$ver3/\"  | awk -F '/' '{ print NR\"：\"\$5 }' | awk -F '：' '/"$cver4："/{ print \$2 }' | awk 'NR==1{print \$1}'"
-			ver4=$(eval $cver4str)
-		if [ "$ver4" == "" ]; then
-			echo "没取到锐速版本，程序出错退出。"
-			exit 1
-		fi
-	fi
-	#根据锐速版本，内核版本，再回头确定使用的大版本。
-	cver1str="cat serverspeederbin.txt | grep '$release/$ver11[^/]*/$ver2/$ver3/$ver4' | awk -F '/' 'NR==1{ print \$2 }'"
-	ver1=$(eval $cver1str)
+	#如果没有匹配到这个版本的锐速，则取第一个
+	serverspeederverstr="grep \"$release/[^/]*/$kernel_result/$bit/\" serverspeederbin.txt | awk -F '/' 'NR==1{print \$5}'"
+	serverspeederver=$(eval $serverspeederverstr)
 fi
 
 
 
-BINFILESTR="cat serverspeederbin.txt | grep '$release/$ver1/$ver2/$ver3/$ver4/0' | awk -F '/' '{ print \$7 }'"
+BINFILESTR="cat serverspeederbin.txt | grep '$release/[^/]*/$kernel_result/$bit/$serverspeederver/0' | awk -F '/' '{ print \$1\"/\"\$2\"/\"\$3\"/\"\$4\"/\"\$5\"/\"\$7 }'"
 BINFILE=$(eval $BINFILESTR)
-BIN="http://soft.91yun.org/soft/serverspeeder/bin/$release/$ver1/$ver2/$ver3/$ver4/$BINFILE"
-echo $BIN
+if [ "$BINFILE" == "" ]; then
+	echo "Unable to get BINFILE. Installation terminated."
+	exit 1
+fi
+BIN=${BINURL}${BINFILE}
 rm -rf serverspeederbin.txt
 
 
 
 
-
-#先取外网ip，根据取得ip获得网卡，然后通过网卡获得mac地址。
-# if [ "$1" == "" ]; then
-	# IP=$(curl ipip.net | awk -F ' ' '{print $2}' | awk -F '：' '{print $2}')
-	# NC="ifconfig | awk -F ' |:' '/$IP/{print a}{a=\$1}'"
-	# NETCARD=$(eval $NC)
-# else
-	# NETCARD=eth0
-# fi
-# MACSTR="LANG=C ifconfig $NETCARD | awk '/HWaddr/{ print \$5 }' "
-# MAC=$(eval $MACSTR)
-# if [ "$MAC" = "" ]; then
-# MACSTR="LANG=C ifconfig $NETCARD | awk '/ether/{ print \$2 }' "
-# MAC=$(eval $MACSTR)
-# fi
-# echo IP=$IP
-# echo NETCARD=$NETCARD
 
 if [ "$1" == "" ]; then
 	MACSTR="LANG=C ifconfig eth0 | awk '/HWaddr/{ print \$5 }' "
@@ -213,72 +142,49 @@ if [ "$1" == "" ]; then
 	if [ "$MAC" == "" ]; then
 		MACSTR="LANG=C ifconfig eth0 | awk '/ether/{ print \$2 }' "
 		MAC=$(eval $MACSTR)
-	fi
+	fi	
 	if [ "$MAC" == "" ]; then
-		#MAC=$(ip link | awk -F ether '{print $2}' | awk NF | awk 'NR==1{print $1}')
-		echo "本破解只支持eth0名的网卡，如果你的网卡不是eth0,请修改网卡名"
+		echo "The name of network interface is not eth0, please retry after changing the name."
 		exit 1
 	fi
 else
 	MAC=$1
-fi
-echo MAC=$MAC
+fi	
 
-#如果自动取不到就要求手动输入
+#如果自动取不到就退出
 if [ "$MAC" = "" ]; then
-echo "无法自动取得mac地址，请手动输入："
-read MAC
-echo "手动输入的mac地址是$MAC"
+	echo "Unable to get MAC address. Installation terminated."
+	exit 1
 fi
 
-
+	
 #下载安装包
-echo "======================================"
-echo "开始下载安装包。。。。"
-echo "======================================"
-wget -N --no-check-certificate -O 91yunserverspeeder.tar.gz  $INSTALLPACK
-tar xfvz 91yunserverspeeder.tar.gz || { echo "下载安装包失败，请检查";exit 1; }
+wget -N --no-check-certificate -O 91yunserverspeeder.tar.gz  $INSTALLPACK 
+tar xfvz 91yunserverspeeder.tar.gz || { echo "Unable to download Installation package. Installation terminated.";exit 1; }
 
 #下载授权文件
-echo "======================================"
-echo "开始下载授权文件。。。。"
-echo "======================================"
-curl "$APX?mac=$MAC" -o 91yunserverspeeder/apxfiles/etc/apx-20341231.lic || { echo "下载授权文件失败，请检查$APX?mac=$MAC";exit 1;}
+wget -N --no-check-certificate -O apx.lic "$APX?mac=$MAC" || { echo "Unable to download lic file, please check: $APX?mac=$MAC";exit 1;}
+mv apx.lic 91yunserverspeeder/apxfiles/etc/
 
 
 #取得序列号
-echo "======================================"
-echo "开始修改配置文件。。。。"
-echo "======================================"
-SNO=$(curl "$APX?mac=$MAC&sno") || { echo "生成序列号失败，请检查";exit 1; }
-echo "序列号：$SNO"
-sed -i "s/serial=\"sno\"/serial=\"$SNO\"/g" 91yunserverspeeder/apxfiles/etc/config
-rv=$release"_"$ver1"_"$ver2
-sed -i "s/Debian_7_3.2.0-4-amd64/$rv/g" 91yunserverspeeder/apxfiles/etc/config
-# sed -i "s/accppp=\"1\"/accppp=\"0\"/g" 91yunserverspeeder/apxfiles/etc/config
 
-#下载bin文件
-echo "======================================"
-echo "开始下载bin运行文件。。。。"
-echo "======================================"
-curl $BIN -o "91yunserverspeeder/apxfiles/bin/acce-3.10.61.0-["$release"_"$ver1"_"$ver2"]" || { echo "下载bin运行文件失败，请检查";exit 1; }
+wget -N --no-check-certificate -O serverspeedersn.txt "$APX?mac=$MAC&sno"
+SNO=$(cat serverspeedersn.txt)
+rm -rf serverspeedersn.txt
+sed -i "s/serial=\"sno\"/serial=\"$SNO\"/g" 91yunserverspeeder/apxfiles/etc/config
+sed -i "s/apx-20341231/apx/g" 91yunserverspeeder/apxfiles/etc/config
+rv=$release"_"$kernel_result
+sed -i "s/acce-3.10.61.0-\[Debian_7_3.2.0-4-amd64\]/acce-$serverspeederver-[$rv]/g" 91yunserverspeeder/apxfiles/etc/config
+
+#下载bin文件;
+wget -N --no-check-certificate -O "acce-"$serverspeederver"-["$release"_"$kernel_result"]" $BIN 
+mv "acce-"$serverspeederver"-["$release"_"$kernel_result"]" 91yunserverspeeder/apxfiles/bin/
 
 #切换目录执安装文件
 cd 91yunserverspeeder
-
-# Restore license permission to read and write if it exist for re-install.
-if [ -f /serverspeeder/etc/apx-20341231.lic ]; then
-    chattr -i /serverspeeder/etc/apx-20341231.lic
-fi
-
 bash install.sh
 
 #禁止修改授权文件
-chattr +i /serverspeeder/etc/apx*
-#CentOS7添加开机启动
-# if [ "$release" == "CentOS" ] && [ "$ver11" == "7" ]; then
-	# chmod +x /etc/rc.d/rc.local
-	# echo "/serverspeeder/bin/serverSpeeder.sh start" >> /etc/rc.local
-# fi
-#安装完显示状态
+#chattr +i /serverspeeder/etc/apx*
 bash /serverspeeder/bin/serverSpeeder.sh status
